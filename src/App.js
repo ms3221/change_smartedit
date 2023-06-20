52795ac14461bbf952a8ad261f25c9fd74c8ffd4
AHN HYEONGJUN <hyeongjun3221@gmail.com>
2023-06-20 13:55:13 +0900

    design 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Button, TextareaAutosize } from "@mui/material";
+import { TextareaAutosize } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import banner from "./src_assets/banner.jpg";
 
 function App() {
   const [cookieValue, setCookieValue] = useState("");
@@ -18,12 +19,6 @@ function App() {
           withCredentials: true,
         }
       );
-
-      //const option = delete data['name']
-      console.log(data);
-      localStorage.setItem("loginCookie", JSON.string);
-      console.log(localStorage.getItem("loginCookie"));
-
       // const cookies = new Cookies();
       // const testCookie = cookies.set(data.name, data.value, {
       //   expires: data.expires,
@@ -53,34 +48,33 @@ function App() {
   }
   return (
     <div className="App">
-      <div className="editor">
-        <div>
-          <p>smartEditor 변환기</p>
-        </div>
-        <div>
-          <Button
-            color="warning"
-            size="large"
-            variant="contained"
-            onClick={handelEdit}
-            style={{ margin: 20 }}
-          >
-            EDIT
-          </Button>
-          <TextareaAutosize
-            placeholder="상품번호를 입력해주세요"
-            style={{
-              width: 500,
-              height: 500,
-              fontSize: 20,
-              fontWeight: "bold",
-            }}
-            onChange={(e) => {
-              setProductsNo(e.target.value);
-            }}
-          ></TextareaAutosize>
-        </div>
+      <div className="banner">
+        <a href="https://www.fastseller.shop/" target="_blank" rel="noreferrer">
+          <img src={banner} className="banner_img"></img>
+        </a>
       </div>
+      <div className="title">
+        <p>스마트에디터 변환 프로그램</p>
+      </div>
+      <div className="product_no_input">
+        <TextareaAutosize
+          placeholder="상품번호를 입력해주세요"
+          style={{
+            width: "100%",
+            height: 800,
+            fontSize: 20,
+            fontWeight: "bold",
+          }}
+          onChange={(e) => {
+            setProductsNo(e.target.value);
+          }}
+          placeholder="&#13;운영하시는 스마트스토어 상품 번호를 입력해주세요.&#13;&#13;한줄에 하나의 상품 번호가 입력되어야 합니다.&#13;예)&#13;&#10;&#10;&#10;8677445649&#13;&#10;&#10;8677445647&#13;&#10;&#10;8677445648&#13;&#13;현재버전은 건기식(식품) 상품만 변환 가능합니다."
+        ></TextareaAutosize>
+      </div>
+      <div className="edit_btn">
+        <button onClick={handelEdit}>변환 시작</button>
+      </div>
+
       {/* <Button color="primary" onClick={handelLogin}>
         naver login
       </Button> */}
